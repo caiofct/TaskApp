958efdbde3890d6c0e032cc57d00c089e92dcc96
Caio Teixeira <caiofct@gmail.com>
2025-03-06 09:00:44 -0300

    Add list scaffold

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,65 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[ show edit update destroy ]
+
+  # GET /lists
+  def index
+    @lists = List.all
+  end
+
+  # GET /lists/1
+  def show
+  end
+
+  # GET /lists/new
+  def new
+    @list = List.new
+  end
+
+  # GET /lists/1/edit
+  def edit
+  end
+
+  # POST /lists
+  def create
+    @list = List.new(list_params)
+
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to @list, notice: "List was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /lists/1
+  def update
+    respond_to do |format|
+      if @list.update(list_params)
+        format.html { redirect_to @list, notice: "List was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /lists/1
+  def destroy
+    @list.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to lists_path, status: :see_other, notice: "List was successfully destroyed." }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_list
+      @list = List.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def list_params
+      params.expect(list: [ :name ])
+    end
+end
