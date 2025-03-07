5c6f12901fc9edefa4fa5a19206bbea59d8936c6
Caio Teixeira <caiofct@gmail.com>
2025-03-07 19:52:27 -0300

    Implement Inline edit for task description

--- app/javascript/controllers/tasks_controller.js
+++ app/javascript/controllers/tasks_controller.js
@@ -2,6 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tasks"
 export default class extends Controller {
+  edit(event) {
+    event.preventDefault();
+    event.target.classList.add("hidden");
+    const descriptionInput = event.target.closest("form").getElementsByClassName("task-description-field")[0];
+    descriptionInput.classList.remove("hidden");
+    descriptionInput.focus();
+  }
+
+  submitEdit(event) {
+    event.preventDefault();
+    event.target.closest("form").requestSubmit();
+  }
+
+  blur(event) {
+    event.preventDefault();
+    event.target.classList.add("hidden");
+    event.target.closest("form").getElementsByClassName("task-description-link")[0].classList.remove("hidden");
+  }
+
   toggle(event) {
     event.target.closest("form").requestSubmit();
   }
